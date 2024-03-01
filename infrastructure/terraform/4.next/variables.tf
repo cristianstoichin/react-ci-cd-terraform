596eb356f76db2e8c2519a70ea6177ff2a1f4694
Cristian Stoichin <virtual.mobile.games@gmail.com>
2024-02-29 17:15:44 -0800

    again

---
+++ infrastructure/terraform/4.next/variables.tf
@@ -0,0 +1,26 @@
+variable "application" {
+  description = "Application name to use"
+  type        = string
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "environment" {
+  description = "Environment name to use"
+  type        = string
+}
+
+variable "hosted_zone_name" {
+  type = string
+}
+
+variable "cert_arn" {
+  type = string
+}
+
+variable "region" {
+  description = "AWS region to use"
+  type        = string
+}
---
+++ infrastructure/terraform/4.next/variables/dev.tfvars
@@ -0,0 +1,6 @@
+region = "us-east-1"
+environment = "dev"
+application = "next"
+bucket_name = "next"
+hosted_zone_name="dash-demo.click"
+cert_arn={CERT_ARN2}
